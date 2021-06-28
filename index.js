c219363152adad2f00ce5cd5e830f1cbe895af3c
xRitchie91 <74946954+xRitchie91@users.noreply.github.com>
2021-06-27 21:00:23 -0500

    added prompts

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
+
+// employee questions function
+const prompts = [
+    {
+        type: 'list',
+        name: 'role',
+        message: 'What\'s your employee\'s role?',
+        choices:
+    }
+]
