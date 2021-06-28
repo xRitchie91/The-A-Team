699198fde4bb9e5a80f4d4f1cba27625b4817f6d
xRitchie91 <74946954+xRitchie91@users.noreply.github.com>
2021-06-28 17:39:53 -0500

    corrected file route

---
+++ engineer.js
@@ -0,0 +1,18 @@
+const Employee = require('./employee');
+
+class Engineer extends Employee {
+    constructor(lastName, firstName, github, id) {
+        super(lastName, firstName, id);
+
+        this.github = github;
+        this.role = 'Engineer';
+        this.icon = 'fas fa-id-badge';
+    };
+
+    getGithub() {
+        return `<span class="has-text-weight-bold">GitHub:</span> <a href="https://github.com/${
+            this.github}" target="_blank" class="has-text-primary">${this.github}</a>`
+    };
+};
+
+module.exports = Engineer;
---
+++ manager.js
@@ -0,0 +1,17 @@
+const Employee = require('./employee');
+
+class Manager extends Employee {
+    constructor(lastName, firstName, id, officeNumber) {
+        super(lastName, firstName, id);
+        
+        this.officeNumber = officeNumber;
+        this.role = 'Manager';
+        this.icon = 'fas fa-user-tie';
+    };
+
+    getOfficeNumber() {
+        return `<span class="has-text-weight-bold">Office Number:</span> ${this.officeNumber}`;
+    };
+};
+
+module.exports = Manager;
