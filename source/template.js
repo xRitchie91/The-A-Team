53cb06184a2bad25b12bc10f4c63418a91e7fe37
xRitchie91 <74946954+xRitchie91@users.noreply.github.com>
2021-06-28 23:58:31 -0500

    Add files via upload

---
+++ source/template.js
@@ -0,0 +1,34 @@
+const formatName = require('../utils/helper');
+const Employee = require('../lib/employee');
+const Manager = require('../lib/manager');
+const Engineer = require('../lib/engineer');
+const Intern = require('../lib/intern');
+
+// addEmployee function
+const addEmployee = employeeInfo => {
+
+    // employee cards
+    let allCards = '';
+
+    employeeInfo.forEach(employee => {    
+        
+        const { firstName, lastName, id, role } = employee;
+        let newEntry = '';
+        let info = '';
+
+        switch (role) {
+            case 'Manager':
+                newEntry = new Manager(formatName(lastName), formatName(firstName), id, employee.officeNumber);
+                info = newEntry.getOfficeNumber();
+                break;
+            case 'Engineer': 
+                newEntry = new Engineer(formatName(lastName), formatName(firstName), id, employee.github);
+                info = newEntry.getGithub();
+                break;
+            case 'Intern':
+                newEntry = new Intern(formatName(lastName), formatName(firstName), id, employee.school);
+                info = newEntry.getSchool();
+                break;
+            default:
+                newEntry = new Employee(formatName(lastName), formatName(firstName), id);
+        };
