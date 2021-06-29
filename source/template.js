c1bcf9ac5225468cf21ed28ef048d5af5ca4cd5e
xRitchie91 <74946954+xRitchie91@users.noreply.github.com>
2021-06-28 22:17:34 -0500

    added require paths

---
+++ source/template.js
@@ -0,0 +1,6 @@
+const formatName = require('../utils/helper');
+const Employee = require('../lib/employee');
+const Manager = require('../lib/manager');
+const Engineer = require('../lib/engineer');
+const Intern = require('../lib/intern');
+
